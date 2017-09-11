841ae9709ce81aded507a79674533ae5f9f658aa
Alexis Henry <henryalexis26@gmail.com>
2017-09-11 16:32:44 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String homePage(Model model){
+        model.addAttribute("myvar", "say hello to the people");
+        return "homeTemplate";
+    }
+}
